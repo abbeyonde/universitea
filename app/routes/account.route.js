6f9e3a0001fa7ae8c324614a5243f61ee3620aa6
abbeyonde <musabsalihin242@gmail.com>
2023-07-19 13:35:10 +0800

    update phase1

--- app/routes/account.route.js
+++ app/routes/account.route.js
@@ -2,8 +2,8 @@ module.exports = app => {
     const accounts = require('../controllers/account.controller');
     var router = require('express').Router();
 
-    router.post('/signup', accounts.create);
-    router.post('/login', accounts.login);
+    router.post('/sign-up', accounts.create);
+    router.post('/sign-in', accounts.login);
 
     app.use('/account', router);
 }
